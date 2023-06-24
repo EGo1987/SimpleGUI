0ca2adeadc9b2d2657572a1dfdff40c50e1128f2
ВашÐEGo87 <gokov87@vk.com>
2023-06-24 22:19:58 +0300

    Initial commit

---
+++ src/com/itproger/Main.java
@@ -0,0 +1,12 @@
+package com.itproger;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ContactForm form = new ContactForm();
+        form.setVisible(true);
+    }
+}
